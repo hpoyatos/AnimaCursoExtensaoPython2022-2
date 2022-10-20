6860e30631cd8160c7129d46917aa368bbba275d
Henrique Poyatos <hpoyatos@yahoo.com.br>
2022-10-20 00:52:58 +0000

    Aprendi a criar variáveis e exibi-la na tela!

--- main.py
+++ main.py
@@ -3,6 +3,18 @@
 # print() = comando de saida
 print("Alo mundo!")
 
+# Quando quiser guardar uma String! (frase)
+nome = "Henrique Poyatos"
+#Quando quiser guardar um número inteiro
+idade = 42
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+
+
+
+
 
 
 ''' 
