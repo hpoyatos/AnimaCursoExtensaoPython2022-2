3c647e414373c899555d0c942af17348f500bff8
Henrique Poyatos <hpoyatos@yahoo.com.br>
2022-11-10 01:18:01 +0000

    Mostramos dois parâmetros com valor padrão

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -33,3 +33,20 @@ valores = [1.99, 24.50, 78.27, 1515.5]
 # se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de .... é ...." (1o. preço, 2o. imposto)
 for valor in valores:
   print(f"O imposto de {valor} é {calcular_imposto(valor)}")
+
+
+#Declarar um função calcular_imposto_aliquota que recebe dois parâmetros: o preço do produto e a alíquota de imposto a ser aplicada e retorna o imposto calculado. Se a aliquota não for informada, utilize 7% como padrão.
+def calcular_imposto_aliquota(valor, aliquota=7):
+  imposto = valor * aliquota / 100
+  return imposto
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor)}")
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 7)}")
+
+#E se agora o imposto for 10%?
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 10)}")
+
